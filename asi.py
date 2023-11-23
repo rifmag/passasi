f0eaf3b33bcac7693f5fa04b73ba1605bb946052
rifmag <142006937+rifmag@users.noreply.github.com>
2023-11-23 11:20:21 +0700

    Update asi.py

--- asi.py
+++ asi.py
@@ -7,13 +7,17 @@ def read_excel_data(file_path):
     data = pd.read_excel(file_path)
     return data
 
-# Fungsi untuk mencari data berdasarkan NIP
+# Fungsi untuk mencari data berdasarkan NIP dan OTP
 def cari_data_berdasarkan_nip_otp(data, nip, otp):
     return data[(data["nip"] == nip) & (data["otp"] == otp)]
 
 def main():
-    st.title("Cek Password")
-    
+    st.title("")
+
+    # Logo in the top-right corner
+    logo = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9L7E2_2WuGtZqM03fu_AcQzq1cdlGrcp_27Vu3W0E1XyfU730QqtbiXNheHLZ4SuMw&usqp=CAU" style="float:left;">'
+    st.markdown(logo, unsafe_allow_html=True)
+
     # Baca data dari file Excel
     data = read_excel_data("dataasi.xlsx")  # Ganti dengan path file Excel Anda
 
@@ -25,9 +29,23 @@ def main():
             student_data = cari_data_berdasarkan_nip_otp(data, int(nip_input), int(otp_input))
             if not student_data.empty:
                 student_name = student_data["Nama"].iloc[0]
-                st.markdown(f"<p style='font-size: 36px; font-weight: bold;'>Nama: {student_name}</p>", unsafe_allow_html=True)
                 student_password = student_data["password"].iloc[0]
-                st.markdown(f"<p style='font-size: 36px; font-weight: bold;'>password: {student_password}</p>", unsafe_allow_html=True)
+
+                # Tampilkan nama dalam kotak bergaris
+                st.markdown(f"""
+                    <div style='border: 2px solid #3498db; padding: 20px; border-radius: 5px;'>
+                        <p style='font-size: 36px; font-weight: bold; margin: 0;'>Nama: {student_name}</p>
+                    </div>
+                """, unsafe_allow_html=True)
+
+                # Tampilkan password dalam kotak bergaris
+                st.markdown(f"""
+                    <div style='border: 2px solid #3498db; padding: 20px; border-radius: 5px;'>
+                        <p style='font-size: 36px; font-weight: bold; margin: 0;'>Password: {student_password}</p>
+                    </div>
+                """, unsafe_allow_html=True)
+
+                # Tambahkan tulisan jika password ditemukan
                 st.success("Password ditemukan. Silahkan masuk melalui tautan [https://upkp2023.asi-asesmen.com/](https://upkp2023.asi-asesmen.com/)")
             else:
                 st.warning("Data tidak ditemukan untuk NIP dan OTP tersebut.")
