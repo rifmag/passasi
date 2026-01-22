bb9c126464a16e535f75f93e3c975e8b708a17db
rifmag <142006937+rifmag@users.noreply.github.com>
2026-01-22 11:43:45 +0700

    Update sertifikat.py

--- asi.py
+++ asi.py
@@ -19,34 +19,47 @@ def main():
     st.markdown(logo, unsafe_allow_html=True)
 
     # Baca data dari file Excel
-    data = read_excel_data("dataasi.xlsx")  # Ganti dengan path file Excel Anda
+    data = read_excel_data("datasertif.xlsx")  # Ganti dengan path file Excel Anda
 
     # Isian NIP dan OTP
     nip_input = st.text_input("Masukkan NIP:", "").strip()
     otp_input = st.text_input("Masukkan OTP:", "").strip()
+    st.markdown("<small>format otp: <b>dd/mm/yyyy</b></small>", unsafe_allow_html=True)
     if st.button("Tampilkan"):
         if nip_input and otp_input:
             student_data = cari_data_berdasarkan_nip_otp(data, int(nip_input), int(otp_input))
             if not student_data.empty:
                 student_name = student_data["Nama"].iloc[0]
-                student_password = student_data["password"].iloc[0]
+                student_link = student_data["Link"].iloc[0]
+                student_ttl = student_data["ttl"].iloc[0]
+                student_jabatan = student_data["jabatan"].iloc[0]
+                student_unit = student_data["unit"].iloc[0]
+                
 
                 # Tampilkan nama dalam kotak bergaris
-                st.markdown(f"""
-                    <div style='border: 2px solid #3498db; padding: 20px; border-radius: 5px;'>
-                        <p style='font-size: 36px; font-weight: bold; margin: 0;'>Nama: {student_name}</p>
-                    </div>
-                """, unsafe_allow_html=True)
+                fields = {
+                    "Nama": student_name,
+                    "TTL": student_ttl,
+                    "Jabatan": student_jabatan,
+                    "Unit": student_unit
+                    }
 
-                # Tampilkan password dalam kotak bergaris
-                st.markdown(f"""
-                    <div style='border: 2px solid #3498db; padding: 20px; border-radius: 5px;'>
-                        <p style='font-size: 36px; font-weight: bold; margin: 0;'>Password: {student_password}</p>
-                    </div>
-                """, unsafe_allow_html=True)
+                with st.container(border=True):
+                    col_l, col_r = st.columns([1, 3])
 
-                # Tambahkan tulisan jika password ditemukan
-                st.success("Password ditemukan. Silahkan masuk melalui tautan [https://upkp2023.asi-asesmen.com/](https://upkp2023.asi-asesmen.com/)")
+                    for label, value in fields.items():
+                        with col_l:
+                            st.markdown(f"**{label}**")
+                        with col_r:
+                            st.markdown(value)
+
+                    st.divider()
+                    st.link_button("🔗 Buka Sertifikat", student_link)
+
+
+
+                # Tambahkan tulisan jika data ditemukan
+                st.success(" Sertifikat ditemukan. Silahkan klik tautan diatas")
             else:
                 st.warning("Data tidak ditemukan untuk NIP dan OTP tersebut.")
 
